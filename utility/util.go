884fac9e264b9a2c318677cffd73b864aeea3648
stonerville-hub <brianstonerville@gmail.com>
2022-02-12 09:12:09 -0500

    Fixed docker compose build

--- utility/util.go
+++ utility/util.go
@@ -3,14 +3,42 @@ package utility
 import (
 	"encoding/json"
 	"fmt"
+	"os"
+	"strconv"
 	"strings"
 
-	aero "github.com/aerospike/aerospike-client-go/v5"
+	as "github.com/aerospike/aerospike-client-go/v5"
 
 	"github.com/google/uuid"
 )
 
-func ConvertToJson(bins aero.BinMap) []byte {
+type Dbconfig struct {
+	Environment string
+	Host        string
+	Port        string
+	Namespace   string
+	Set         string
+	Client      *as.Client
+}
+
+const (
+	ENVIROMENT  = "ENVIROMENT"
+	HOST        = "HOST"
+	PORT        = "PORT"
+	NAMESPACE   = "NAMESPACE"
+	SET         = "SET"
+	APIKEY      = "api_key"
+	FIRSTNAME   = "first_name"
+	LASTNAME    = "last_name"
+	COMPANY     = "company"
+	defaultEnv  = "dev"
+	defaultHost = "aerospike"
+	defaultPort = "3000"
+	defaultNS   = "test"
+	defaultSet  = "users"
+)
+
+func ConvertToJson(bins as.BinMap) []byte {
 	jsonValue, err := json.Marshal(bins)
 	PanicOnError(err)
 	return jsonValue
@@ -30,3 +58,33 @@ func PanicOnError(err error) {
 	}
 }
 
+func GetConnection() *Dbconfig {
+	host := GetEnv(HOST, defaultHost)
+	port := GetEnv(PORT, defaultPort)
+	client := retrieveClient(host, port)
+	dbconfig := &Dbconfig{GetEnv(ENVIROMENT, defaultEnv), host, port, GetEnv(NAMESPACE, defaultNS), GetEnv(SET, defaultSet), client}
+	return dbconfig
+}
+
+func retrieveClient(host string, port string) *as.Client {
+	errDBDown := "Database is DOWN"
+	_port, err := strconv.Atoi(port)
+	PanicOnError(err)
+	client, err := as.NewClient(host, _port)
+	if err != nil {
+		LogMessage("*******WARNING*************")
+		LogMessage(errDBDown+" for "+host+":"+port)
+		LogMessage("*******WARNING*************")
+		panic(errDBDown)
+	}
+	LogMessage("Database is UP for "+host+":"+port)
+
+	return client
+}
+
+func GetEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
